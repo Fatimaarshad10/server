f2a4533b6022bda955a6790cbb5e88cdaee49bbe
Fatimaarshad10 <fatimaarshad091@gmail.com>
2023-12-04 00:54:30 +0500

    vercel

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ require("dotenv").config();
 
 // middlewares
 app.use(express.json());
-app.use('/student', studentRoute)
+app.get('/student',(req,res)=>{
+  res.json("hello")
+} )
 
 const PORT = process.env.PORT || 4000;
 // connect with mongodb 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Mongo Connection Built")
-    app.listen(PORT, () => {
-      console.log(`Server listening on http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+// mongoose.connect(process.env.MONGO_URL)
+//   .then(() => {
 
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
+
+  console.log("Mongo Connection Built")
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
