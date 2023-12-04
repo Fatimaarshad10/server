1a6ded25ae72151aa4d274bd0790e62da1a0f985
Fatimaarshad10 <fatimaarshad091@gmail.com>
2023-12-04 12:14:51 +0500

    vercel

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const studentRoute = require('./routes/studentRoute')
 const app = express();
 require("dotenv").config();
 
-
-
 // middlewares
 app.use(express.json());
 app.use('/student' , studentRoute);
-
-
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 // connect with mongodb 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
-
 console.log("Mongo Connection Built")
 app.listen(process.env.PORT , () => {
   console.log(`Server listening on http://localhost:${process.env.PORT }`);
@@ -23,3 +22,4 @@ app.listen(process.env.PORT , () => {
   .catch((error) => {
     console.log(error.message);
   });
+  module.exports = app;
